f4bdc7129b27544e415022b72dbb6b9c1b47386f
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-06 15:57:17 +0300

    1st answer

--- C#-04/Program.cs
+++ C#-04/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1 
+            //double[] price = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(price[1]); 
+            #endregion
         }
     }
 }
