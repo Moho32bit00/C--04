ff527b3249bae0fd10907cf1cc7c14d6c2db567b
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-06 15:58:14 +0300

    2nd answer

--- C#-04/Program.cs
+++ C#-04/Program.cs
@@ -8,6 +8,19 @@
             //double[] price = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(price[1]); 
             #endregion
+            #region q2 
+            //int[,] a = { { 3 , 5 } ,
+            //    { 1 , 4 } };
+            //for (int i = 0; i < a.GetLength(0); i++)
+            //{
+            //    Console.Write($"copies on  shelf {i}:");
+            //    for (int j = 0;  j < a.GetLength(1); j++)
+            //    {
+            //        Console.Write($" {a[i , j]}  ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
         }
     }
 }
